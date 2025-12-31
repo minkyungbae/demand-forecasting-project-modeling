518c08d121abc084a1ca36a1240c8a8245ae3e0d
minkyungbae <bmk0703@naver.com>
2025-12-31 15:03:22 +0900

    Update: 주석 추가

--- frontend/src/layout/Dashboard/index.jsx
+++ frontend/src/layout/Dashboard/index.jsx
@@ -30,7 +30,7 @@ export default function DashboardLayout() {
   return (
     <Box sx={{ display: 'flex', width: '100%' }}>
       <Header />
-      <Drawer />
+      <Drawer /> {/* 왼쪽 사이드바(메뉴바 */}
 
       <Box component="main" sx={{ width: 'calc(100% - 260px)', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
         <Toolbar sx={{ mt: 'inherit' }} />
