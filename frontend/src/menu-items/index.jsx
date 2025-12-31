bdc9cc089a767a8065cf92b6f41ebd6a52cbbee6
minkyungbae <bmk0703@naver.com>
2025-12-31 16:25:42 +0900

    Update: menu items에 functionMenu 추가

--- frontend/src/menu-items/index.jsx
+++ frontend/src/menu-items/index.jsx
@@ -3,11 +3,12 @@ import dashboard from './dashboard';
 import pages from './page';
 import utilities from './utilities';
 import support from './support';
+import functionMenu from './function'; // 251231 추가
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, pages, utilities, support]
+  items: [dashboard, pages, functionMenu, utilities, support] // 251231 추가
 };
 
 export default menuItems;
